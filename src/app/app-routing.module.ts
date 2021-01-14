046299a27bc007152b8962fcdc515cbcc1575d7d
Sasi Matcha <sasi@sasis-mbp.lan>
2021-01-14 16:28:14 -0600

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AddPartComponent} from "./parts/add-part/add-part.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'list-parts', redirectTo: '/', pathMatch: 'full'},
+  {path: 'add-part', component: AddPartComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import { FormsModule } from '@angular/forms'; 
+import {HttpClientModule} from "@angular/common/http";
+import {AddPartComponent} from "./parts/add-part/add-part.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"; 
+import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
+import { ExcelService } from './parts/excel.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPartComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    BsDatepickerModule.forRoot()
   ],
-  providers: [],
+  providers: [ExcelService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
